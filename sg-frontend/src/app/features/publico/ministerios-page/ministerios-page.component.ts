cf2d39b4ce6f1d2dd77bb0c9c6f2e25b7e2ce4c4
Hugo Santos Pereira <32572028+HugoSantosp@users.noreply.github.com>
2026-09-17 23:39:19 -0300

    Import getApiUrl from backend-config

--- sg-frontend/src/app/features/publico/ministerios-page/ministerios-page.component.ts
+++ sg-frontend/src/app/features/publico/ministerios-page/ministerios-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { getApiUrl } from '../../../core/config/backend-config';
 
 @Component({
   selector: 'app-ministerios-page',
