5464e0994b74503772fd31e44169252a9b7cc757
Hugo Santos Pereira <32572028+HugoSantosp@users.noreply.github.com>
2026-09-17 22:37:24 -0300

    Update production API URL

--- sg-frontend/projects/meu-ministerio/src/environments/environment.prod.ts
+++ sg-frontend/projects/meu-ministerio/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // URL do backend — em produção injetada via window.__BACKEND_URL__
-  apiUrl: 'https://sge-sistema-de-gestao-eclesiastico.onrender.com'
+  apiUrl: 'https://api.icertag.com.br'
 };
